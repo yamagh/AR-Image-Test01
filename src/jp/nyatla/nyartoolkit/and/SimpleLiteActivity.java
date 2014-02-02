b397c71435d465fef76306fa8f2bd818ffccad9e
ygmh <hiroyuki.yamagami@gmail.com>
2014-02-02 20:33:08 +0900

    first commit
    my first commit

---
+++ src/jp/nyatla/nyartoolkit/and/SimpleLiteActivity.java
@@ -0,0 +1,121 @@
+package jp.nyatla.nyartoolkit.and;
+
+import javax.microedition.khronos.opengles.GL10;
+
+
+import my.test.gl.GlTest01;
+//import jp.androidgroup.nyartoolkit.R;
+import jp.androidgroup.nyartoolkit.markersystem.NyARAndMarkerSystem;
+import jp.androidgroup.nyartoolkit.markersystem.NyARAndSensor;
+import jp.androidgroup.nyartoolkit.sketch.AndSketch;
+import jp.androidgroup.nyartoolkit.utils.camera.CameraPreview;
+import jp.androidgroup.nyartoolkit.utils.gl.*;
+import jp.nyatla.nyartoolkit.markersystem.NyARMarkerSystemConfig;
+import android.content.res.AssetManager;
+import android.hardware.Camera;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.FrameLayout;
+
+/**
+ * Hiroマーカの上にカラーキューブを表示します。
+ * 定番のサンプルです。
+ *
+ */
+public class SimpleLiteActivity extends AndSketch implements AndGLView.IGLFunctionEvent
+{
+	CameraPreview _camera_preview;
+	AndGLView _glv;
+	Camera.Size _cap_size;
+	/**
+	 * onStartでは、Viewのセットアップをしてください。
+	 */
+	@Override
+	public void onStart()
+	{
+		super.onStart();
+		FrameLayout fr=((FrameLayout)this.findViewById(R.id.sketchLayout));
+		//カメラの取得
+		this._camera_preview=new CameraPreview(this);
+		this._cap_size=this._camera_preview.getRecommendPreviewSize(320,240);
+		//画面サイズの計算
+		int h = this.getWindowManager().getDefaultDisplay().getHeight();
+		int screen_w,screen_h;
+		screen_w=(this._cap_size.width*h/this._cap_size.height);
+		screen_h=h;
+		//camera
+		fr.addView(this._camera_preview, 0, new LayoutParams(screen_w,screen_h));
+		//GLview
+		this._glv=new AndGLView(this);
+		fr.addView(this._glv, 0,new LayoutParams(screen_w,screen_h));
+	}
+
+	NyARAndSensor _ss;
+	NyARAndMarkerSystem _ms;
+	private int _mid;
+	AndGLTextLabel text;
+	AndGLBox box;
+	AndGLFpsLabel fps;
+	
+	GlTest01 glt01;  // Add yama
+	
+	public void setupGL(GL10 gl)
+	{
+		try
+		{
+			AssetManager assetMng = getResources().getAssets();
+			//create sensor controller.
+			this._ss=new NyARAndSensor(this._camera_preview,this._cap_size.width,this._cap_size.height,30);
+			//create marker system
+			this._ms=new NyARAndMarkerSystem(new NyARMarkerSystemConfig(this._cap_size.width,this._cap_size.height));
+			this._mid=this._ms.addARMarker(assetMng.open("AR/data/hiro.pat"),16,25,80);
+			this._ss.start();
+			//setup openGL Camera Frustum
+			gl.glMatrixMode(GL10.GL_PROJECTION);
+			gl.glLoadMatrixf(this._ms.getGlProjectionMatrix(),0);
+			this.text=new AndGLTextLabel(this._glv);
+			this.box=new AndGLBox(this._glv,40);
+			this.glt01 = new GlTest01(this._glv, gl);  // Add yama
+			this._debug=new AndGLDebugDump(this._glv);
+			this.fps=new AndGLFpsLabel(this._glv,"MarkerPlaneActivity");
+			this.fps.prefix=this._cap_size.width+"x"+this._cap_size.height+":";
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			this.finish();
+		}
+	}
+	AndGLDebugDump _debug=null;
+	/**
+	 * 継承したクラスで表示したいものを実装してください
+	 * @param gl
+	 */
+	public void drawGL(GL10 gl)
+	{
+		try{
+			//背景塗り潰し色の指定
+			gl.glClearColor(0,0,0,0);
+	        //背景塗り潰し
+	        gl.glClear(GL10.GL_COLOR_BUFFER_BIT|GL10.GL_DEPTH_BUFFER_BIT);
+	        if(ex!=null){
+	        	_debug.draw(ex);
+	        	return;
+	        }
+	        fps.draw(0, 0);
+			synchronized(this._ss){
+				this._ms.update(this._ss);
+				if(this._ms.isExistMarker(this._mid)){
+			        this.text.draw("found"+this._ms.getConfidence(this._mid),0,16);
+					gl.glMatrixMode(GL10.GL_MODELVIEW);
+					gl.glLoadMatrixf(this._ms.getGlMarkerMatrix(this._mid),0);
+//					this.box.draw(0,0,20);  // Del yama
+					this.glt01.draw(0, 0, 20);  // add yama
+				}
+		}
+		}catch(Exception e)
+		{
+			ex=e;
+		}
+	}
+	Exception ex=null;
+}
---
+++ src/my/test/gl/GlTest01.java
@@ -0,0 +1,125 @@
+package my.test.gl;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import javax.microedition.khronos.opengles.GL10;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.opengl.GLU;
+import android.opengl.GLUtils;
+import android.util.Log;
+import jp.androidgroup.nyartoolkit.utils.gl.AndGLView;
+import jp.nyatla.nyartoolkit.and.R;
+
+/**
+ * 画像表示のクラス
+ * @author yama
+ *
+ */
+public class GlTest01 implements AndGLView.IGLViewEventListener {
+
+	// ログ出力用
+	private static final String TAG = "GlTest01";
+	
+	private GL10 _ref_gl;
+
+	// 頂点座標
+	private float apexs[] = new float[] { -20f, -20f, 20f, -40f,   -20f, 20f, 40f, 20f, };
+//	private float apexs[] = new float[] { -1f, -1f, 1f, -1f, -1f, 1f, 1f, 1f, };
+
+	// 頂点座標バッファ
+	private FloatBuffer apexBuff;
+
+	// 頂点テクスチャ
+	private float coords[] = new float[] {
+			// 上下反転
+			0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, };
+	// 頂点テクスチャバッファ
+	private FloatBuffer coordsBuff;
+
+	private int[] textures = new int[1];
+	private int textureId = 0;
+
+	public GlTest01(AndGLView i_context, GL10 gl) {
+		i_context._evl.add(this);
+
+		this._ref_gl = gl;
+
+		apexBuff = makeFloatBuffer(apexs);
+		coordsBuff = makeFloatBuffer(coords);
+
+		// テクスチャ管理番号割り当て
+		gl.glDeleteTextures(1, textures, 0);
+		gl.glGenTextures(1, textures, 0);
+		textureId = textures[0];
+
+		// テクスチャ管理番号バインド
+		gl.glBindTexture(GL10.GL_TEXTURE_2D, textureId);
+
+		Bitmap bitmap = BitmapFactory.decodeResource(i_context.getResources(), R.drawable.dog);
+		Log.v(TAG, "" + bitmap.getHeight());
+		GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, bitmap, 0);
+//		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_NEAREST);
+		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_LINEAR);
+//		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_NEAREST);
+		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
+		bitmap.recycle();
+	}
+
+	public void draw(float i_x, float i_y, float i_z) {
+		Log.v(TAG, "draw...");
+		GL10 gl = this._ref_gl;
+		gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		gl.glPushMatrix();
+//		GLU.gluLookAt(gl, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+		GLU.gluLookAt(gl, i_x, i_y, 3, i_x, i_y, 0, 0, 1, 0);
+		gl.glPushMatrix();
+		gl.glVertexPointer(2, GL10.GL_FLOAT, 0, apexBuff);
+
+		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+
+		// テクスチャ配列の指定
+		gl.glBindTexture(GL10.GL_TEXTURE_2D, textureId);
+		gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, coordsBuff);
+
+		// テクスチャーの有効化
+		gl.glEnable(GL10.GL_TEXTURE_2D);
+		gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+
+		// テクスチャ描画
+		gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, 4);
+
+		// テクスチャーの無効化
+		gl.glDisableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+		gl.glDisable(GL10.GL_TEXTURE_2D);
+		gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+
+		gl.glPopMatrix();
+		gl.glPopMatrix();
+
+	}
+
+	@Override
+	public void onGlChanged(GL10 i_gl, int i_width, int i_height) {
+		if (this._ref_gl != null) {
+		}
+		this._ref_gl = i_gl;
+	}
+
+	@Override
+	public void onGlMayBeStop() {
+		this._ref_gl = null;
+	}
+
+	// 頂点の配列をバッファーに変換するメソッド
+	private static FloatBuffer makeFloatBuffer(float[] values) {
+		ByteBuffer bb = ByteBuffer.allocateDirect(values.length * 4);
+		bb.order(ByteOrder.nativeOrder());
+		FloatBuffer fb = bb.asFloatBuffer();
+		fb.put(values);
+		fb.position(0);
+		return fb;
+	}
+
+}
